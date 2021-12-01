d21cc135bed09201825ae4229d2d1a781c2bd3cc
Thomas Heaton <thomas.m.heaton@gmail.com>
2021-12-01 12:14:43 +0000

    day one part two complete

--- one/one.py
+++ one/one.py
@@ -8,8 +8,7 @@ if __name__ == "__main__":
 
     with open("input.txt", "r") as input_file:
         for line in input_file.readlines():
-            x = int(line)
-            current = x
+            current = int(line)
             if current > previous != 0:
                 count += 1
             previous = current
--- one/two.py
+++ one/two.py
@@ -6,12 +6,20 @@ if __name__ == "__main__":
     previous: int = 0
     current: int = 0
 
-    with open("input.txt", "r") as input_file:
-        for line in input_file.readlines():
-            x = int(line)
-            current = x
-            if current > previous != 0:
-                count += 1
-            previous = current
+    three_list: [int] = []
 
+    with open("input.txt", "r") as input_file:
+        for index, line in enumerate(lines := input_file.readlines()):
+            try:
+                three_list.append(int(lines[index]) + int(lines[index + 1]) + int(lines[index + 2]))
+            except IndexError as e:
+                pass
+
+    for number in three_list:
+        current = number
+        if current > previous != 0:
+            count += 1
+        previous = current
+
+    print(f'{three_list = }')
     print(f'{count = }')
