94c40540a77ef9d0c0cb5734645543e740dd4307
Thomas Heaton <thomas.m.heaton@gmail.com>
2021-12-01 11:35:42 +0000

    day one start

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from util import util
 
 if __name__ == '__main__':
     print('Advent of Code 2021')
-    print(f'Inputs: \n{util.get_inputs(1)}')
+    # print(f'Inputs: \n{util.get_inputs(1)}')
---
+++ one/one.py
@@ -0,0 +1,5 @@
+
+count: int = 0
+
+with open("input.txt", "r") as input_file:
+    pass
