b25a643d972103c3c40b6f946e27c8503c79c455
Thomas Heaton <thomas.m.heaton@gmail.com>
2021-12-16 13:59:34 +0000

    start day sixteen

---
+++ 16-sixteen/one.py
@@ -0,0 +1,9 @@
+# Day Sixteen - Part One
+
+
+if __name__ == "__main__":
+
+    with open("input.txt", "r") as input_file:
+
+        for line in [x.strip("\n") for x in input_file.readlines()]:
+            print(f'{line = }')
