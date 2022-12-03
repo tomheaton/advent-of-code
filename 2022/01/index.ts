5db3f1ddfd803692c0fc1eadad1ba67d9851bb96
Tom Heaton <thomas.m.heaton@gmail.com>
2022-12-03 23:41:26 +0000

    read files

--- 2022/01/index.ts
+++ 2022/01/index.ts
@@ -1,1 +1,12 @@
+import {readFileSync} from "fs";
+
+const getInputs = (): string[] => {
+    let data = readFileSync("./input.txt", {encoding: "utf-8"})
+    if (!data) throw new Error("no data found");
+    return data.toString().split("/r/n");
+}
+
+
 console.log("01");
+
+console.log(getInputs());
