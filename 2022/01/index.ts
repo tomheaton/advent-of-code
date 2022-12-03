eddb9077b2793b71be5e9e667cb46273d87c1b2e
Tom Heaton <thomas.m.heaton@gmail.com>
2022-12-03 23:50:24 +0000

    f

--- 2022/01/index.ts
+++ 2022/01/index.ts
@@ -3,10 +3,22 @@ import {readFileSync} from "fs";
 const getInputs = (): string[] => {
     let data = readFileSync("./input.txt", {encoding: "utf-8"})
     if (!data) throw new Error("no data found");
-    return data.toString().split("/r/n");
+    return data.toString().split("\r\n");
 }
 
 
 console.log("01");
 
 console.log(getInputs());
+
+let answer = 0;
+
+getInputs().forEach((calories, index) => {
+    if (!calories) {
+        answer = 0;
+        return;
+    }
+    answer += Number(calories);
+});
+
+console.log("answer: " + answer);
