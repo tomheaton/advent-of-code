86dd5d0ef8b61a02d38a0b976013faf1052e4b45
Thomas Heaton <thomas.m.heaton@gmail.com>
2022-12-22 19:08:06 +0000

    fix inputs

--- 2022/21/index.ts
+++ 2022/21/index.ts
@@ -3,7 +3,7 @@ import {readFileSync} from "fs";
 const getInputs = (test?: boolean): string[] => {
     let data = readFileSync(`./input${test ? ".test" : ""}.txt`, {encoding: "utf-8"})
     if (!data) throw new Error("no data found");
-    return data.toString().split("\r\n");
+    return data.toString().split("\n");
 }
 
 console.log("21");
