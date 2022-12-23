90b868657e8f035d4144079472cc6b8372bef111
Thomas Heaton <thomas.m.heaton@gmail.com>
2022-12-23 11:51:40 +0100

    prep day 06

---
+++ 2022/06/index.ts
@@ -0,0 +1,21 @@
+import {readFileSync} from "fs";
+
+const getInputs = (test?: boolean): string[] => {
+  let data = readFileSync(`./input${test ? ".test" : ""}.txt`, {encoding: "utf-8"});
+  if (!data) throw new Error("no data found");
+  return data.toString().split("\n");
+}
+
+console.log("06");
+
+console.log("part a");
+
+getInputs(true).forEach((row) => {
+  console.log(row);
+});
+
+// console.log("part b");
+
+// getInputs(true).forEach((row) => {
+//   console.log(row);
+// });
