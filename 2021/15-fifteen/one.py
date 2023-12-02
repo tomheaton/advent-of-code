3cf109cd1e692504037d0edd7fd80b57f77cc939
Tom Heaton <thomas.m.heaton@gmail.com>
2023-12-02 15:53:36 +0000

    remove old days

--- 2021/15-fifteen/one.py
+++
@@ -1,9 +0,0 @@
-# Day Fifteen - Part One
-
-
-if __name__ == "__main__":
-
-    with open("input.txt", "r") as input_file:
-
-        for line in [x.strip("\n") for x in input_file.readlines()]:
-            print(f'{line = }')
--- 2022/21/index.ts
+++
@@ -1,9 +0,0 @@
-import {readFileSync} from "node:fs";
-
-const getInputs = (test?: boolean): string[] => {
-    let data = readFileSync(`./input${test ? ".test" : ""}.txt`, {encoding: "utf-8"});
-    if (!data) throw new Error("no data found");
-    return data.toString().split("\n");
-}
-
-console.log("21");
