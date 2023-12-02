097081585dd9e977042dae3e25a44e78aad25dc9
Tom Heaton <thomas.m.heaton@gmail.com>
2023-12-02 15:45:02 +0000

    day 2 pt 2

--- 2023/examples/2.rs
+++ 2023/examples/2.rs
@@ -14,21 +14,16 @@ fn part_1() {
 
     for line in input.lines() {
         let game = line.split(": ").collect::<Vec<&str>>();
-        // println!("{:?}", game);
 
         // TODO: game id is just index + 1 so this is unnecessary
         let game_id_str = game[0].split(" ").collect::<Vec<&str>>()[1];
         let game_id = str::parse::<i32>(&game_id_str).unwrap();
-        println!("\nGame: {:?}", game_id);
 
         let sets = game[1].split("; ").collect::<Vec<&str>>();
-        // println!("{:?}", sets);
 
         let mut impossible = false;
 
         for set in sets.iter() {
-            println!("set: {:?}", set);
-
             let picks = set.split(", ").collect::<Vec<&str>>();
 
             for pick in picks.iter() {
@@ -59,7 +54,6 @@ fn part_1() {
         }
 
         if !impossible {
-            println!("Game {} is possible", game_id);
             answer += game_id;
         }
     }
@@ -68,9 +62,55 @@ fn part_1() {
 }
 
 fn part_2() {
-    let input = aoc_2023::get_input(2, true);
+    let input = aoc_2023::get_input(2, false);
 
     let mut answer = 0;
+
+    for line in input.lines() {
+        let game = line.split(": ").collect::<Vec<&str>>();
+
+        // TODO: game id is just index + 1 so this is unnecessary
+        let game_id_str = game[0].split(" ").collect::<Vec<&str>>()[1];
+        let game_id = str::parse::<i32>(&game_id_str).unwrap();
+
+        let sets = game[1].split("; ").collect::<Vec<&str>>();
+
+        let mut red_min = 0;
+        let mut green_min = 0;
+        let mut blue_min = 0;
+
+        for set in sets.iter() {
+            let picks = set.split(", ").collect::<Vec<&str>>();
+
+            for pick in picks.iter() {
+                let data = pick.split(" ").collect::<Vec<&str>>();
+
+                let cubes = str::parse::<i32>(&data[0]).unwrap();
+                let colour = data[1];
+
+                match colour {
+                    "red" => {
+                        if cubes > red_min {
+                            red_min = cubes;
+                        }
+                    }
+                    "green" => {
+                        if cubes > green_min {
+                            green_min = cubes;
+                        }
+                    }
+                    "blue" => {
+                        if cubes > blue_min {
+                            blue_min = cubes;
+                        }
+                    }
+                    _ => panic!("Unknown colour"),
+                }
+            }
+        }
+
+        answer += red_min * green_min * blue_min;
+    }
 
     println!("Part 2: {}", answer);
 }
