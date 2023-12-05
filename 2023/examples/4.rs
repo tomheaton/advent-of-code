fa5ddd7f59b6308457052412042cb9630fd09426
Tom Heaton <thomas.m.heaton@gmail.com>
2023-12-05 02:47:40 +0000

    day 4 pt 2

--- 2023/examples/4.rs
+++ 2023/examples/4.rs
@@ -1,6 +1,6 @@
 fn main() {
     let day = 4;
-    let input = aoc_2023::get_input(day, true);
+    let input = aoc_2023::get_input(day, false);
     println!("Day {}", day);
     part_1(input.clone());
     part_2(input.clone());
@@ -37,7 +37,39 @@ fn part_1(input: String) {
 }
 
 fn part_2(input: String) {
-    let mut answer = 0;
+    let mut answer = input.lines().count();
+
+    let mut card_numbers_to_play =
+        (1..=input.lines().count()).collect::<Vec<usize>>();
+
+    while card_numbers_to_play.len() > 0 {
+        let card_number = card_numbers_to_play.pop().unwrap();
+        let line = input.lines().nth(card_number - 1).unwrap();
+
+        let data = line.split([':', '|']).collect::<Vec<&str>>();
+
+        let winning_numbers = data[1]
+            .split_whitespace()
+            .map(|x| x.parse::<i32>().unwrap())
+            .collect::<Vec<i32>>();
+
+        let numbers = data[2]
+            .split_whitespace()
+            .map(|x| x.parse::<i32>().unwrap())
+            .collect::<Vec<i32>>();
+
+        let matches = numbers
+            .iter()
+            .filter(|x| winning_numbers.contains(x))
+            .collect::<Vec<&i32>>();
+
+        let card_ids_won = ((card_number + 1)..card_number + matches.len() + 1)
+            .collect::<Vec<usize>>();
+
+        answer += card_ids_won.len();
+
+        card_numbers_to_play.extend(card_ids_won);
+    }
 
     println!("Part 2: {}", answer);
 }
