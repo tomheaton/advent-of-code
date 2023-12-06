f3c570ed829b9b8203783595818b34bb3d431524
Tom Heaton <thomas.m.heaton@gmail.com>
2023-12-06 06:17:23 +0000

    day 6 pt 1

--- 2023/examples/6.rs
+++ 2023/examples/6.rs
@@ -15,13 +15,40 @@ fn main() {
 }
 
 fn part_1(input: String) {
-    let mut answer = 0;
+    let mut answer = 1;
+
+    let time_data = input.lines().nth(0).unwrap();
+    let distance_data = input.lines().nth(1).unwrap();
+
+    let times = time_data.split_whitespace().skip(1).collect::<Vec<&str>>();
+    let distances = distance_data
+        .split_whitespace()
+        .skip(1)
+        .collect::<Vec<&str>>();
+
+    for i in 0..times.len() {
+        let mut ways_to_beat_record = Vec::new();
+
+        let time = times[i].parse::<i32>().unwrap();
+        let distance = distances[i].parse::<i32>().unwrap();
+
+        for ms in 0..time {
+            let speed = ms;
+            let time_left = time - ms;
+
+            if speed * time_left > distance {
+                ways_to_beat_record.push(ms);
+            }
+        }
+
+        answer *= ways_to_beat_record.len();
+    }
 
     println!("Part 1: {}", answer);
 }
 
 fn part_2(input: String) {
-    let mut answer = 0;
+    let mut answer = 1;
 
     println!("Part 2: {}", answer);
 }
