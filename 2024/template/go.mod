2b22a3ef3a7d1dcf816c9789a24f77d7461430b8
Tom Heaton <thomas.m.heaton@gmail.com>
2024-12-07 23:45:14 +0000

    2024: template

---
+++ 2024/template/go.mod
@@ -0,0 +1,3 @@
+module advent-of-code-2024-day-x
+
+go 1.23.0
---
+++ 2024/template/main.go
@@ -0,0 +1,23 @@
+package main
+
+// Advent of Code 2024 day x
+// https://adventofcode.com/2024/day/x
+
+import (
+	_ "embed"
+)
+
+//go:embed 1.txt
+var input1 string
+
+//go:embed 1.txt
+var input2 string
+
+func main() {
+	part1()
+	part2()
+}
+
+func part1() {}
+
+func part2() {}
